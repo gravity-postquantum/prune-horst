67835e10783f83c885f77a9887455174ac9c9944
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2017-09-26 14:44:54 +0200

    KAT generation bug fix, now using NIST deterministic randombytes()

--- Reference_Implementation/randombytes.h
+++ Reference_Implementation/randombytes.h
@@ -1,13 +1,7 @@
+/*
+ * Copyright (C) 2017 Nagravision S.A.
+ */
+
 #pragma once
 
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
-
-/* lazy unsafe emulation of SUPERCOP's randombytes() */
-static void randombytes (unsigned char *x, unsigned long long xlen) {
-    static int fd = -1;
-    if (fd == -1) fd = open ("/dev/urandom", O_RDONLY);
-    read (fd, x, xlen);
-}
+void randombytes (unsigned char *x, unsigned long long xlen);
